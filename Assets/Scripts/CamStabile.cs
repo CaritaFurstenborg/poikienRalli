408e3dbe5ba7a5755e0aa84ee8f987bd5876f45a
Carita Furstenborg <carita.furstenborg@gmail.com>
2018-05-22 13:30:47 +0300

    initial commit

---
+++ Assets/Scripts/CamStabile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamStabile : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject theCar;
+
+    private float CamX, CamY, CamZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CamX = theCar.transform.eulerAngles.x;
+        CamY = theCar.transform.eulerAngles.y;
+        CamZ = theCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(CamX - CamX, CamY, CamZ - CamZ);
+	}
+}
